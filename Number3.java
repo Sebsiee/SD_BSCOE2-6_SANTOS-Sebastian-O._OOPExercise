8ef8091a3d84b9fd246ba6c6a1020d4a76b90ba2
unknown <santosbasty4@gmail.com>
2023-06-09 20:36:19 +0800

    Number 2

---
+++ Number3.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        int t1 = 0, t2 = 1;
+        
+        System.out.print("First " + n + " terms: ");
+
+        /*
+        insert your code here
+        */
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(t1 + " ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
